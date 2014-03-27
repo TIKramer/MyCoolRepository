38a7233674eadcaa0c61b8a062fe747017851c63
Joshua Danks <041019162@central.wa.edu.au>
2014-03-27 11:54:33 +0800

    CoolestSummaryOceanic

--- DownloadThisThingIMade/DownloadThisThingIMade/Form1.vb
+++ DownloadThisThingIMade/DownloadThisThingIMade/Form1.vb
@@ -7,8 +7,11 @@
 
         ListBox1.Items.Add("Thomas")
         ' ListBox1.Items.Add("Your Name")
-
+        ListBox1.Items.Add("Joshua <]8^)")
     End Sub
 
   
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
 End Class
